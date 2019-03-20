1d7b7a6c180ddfa46522d35ccdf98d586511bce7
xbetik <andrej.betik1@gmail.com>
2019-03-20 23:08:04 +0100

    input format prototyp added

---
+++ navrh_na_format.js
@@ -0,0 +1,17 @@
+{
+  "i" : "1",
+  "n" : "Root",
+  "l" : "A",
+  "c": [ {
+	    "i" : "11",
+	    "n" : "LeftChildOfRoot",
+	    "l" : "B",
+	 },	
+
+	 {
+	    "i" : "12",
+	    "n" : "RightChildOfRoot",
+	    "l" : "B",
+	 }
+       ]
+}
