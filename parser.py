4d591d6ef53983fb5211c8873d6cbb041024e32d
xbetik <andrej.betik1@gmail.com>
2019-05-02 00:58:02 +0200

    Fixed a lot of bugs, Added features: right label, description from input file, resizing tree, animation and step is one function, with/w/0 background tree, animation/stop button and some more

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 class Tree:
     def __init__(self):
@@ -11,55 +12,75 @@ class Node:
         self.orderRank = None
         self.nodeType = None
         self.color = None
-        self.label = None
+        self.label1 = None
+        self.label2 = None
         self.children = []
+        
+def parseLabels(line):
+    splitter = " " + '"'
+    result = line.split(splitter)
+    argLen = len(result)
+    args = None
+    label1 = None
+    label2 = None
+    args = result[0]
+    if argLen > 1:
+        label1 = result[1]
+    if argLen > 2:
+        label2 = result[2]
+        
+    return args, label1, label2
+
+def split_path_and_arguments(line):
+    s = line.split(" ")
+    path = s[0]
+    args = s[1:]
+    return path, args
         
 def readData(fileName):
     f = open(fileName, "r")
     r = f.read().splitlines()
-    lines = []
     comments = []
+    data = []
     for l in r:
         if l[0] == '#':
             comments.append(l[1:])
         else:
-            s = l.split(",")   
-            lines.append(s)
-    return lines, comments
+            path_and_args, label1, label2 = parseLabels(l)
+            path, args = split_path_and_arguments(path_and_args)
+            data.append((path, args, label1, label2))
+    return data, comments
 
-def parse(l):
-    value = l[0]
-    params = l[1]
-    p = params.split(" ")
-    name = p[0]
-    orderRank = p[1]
-    nodeType, color, label = None, None, None
-    if len(p) > 2:
-        nodeType = p[2]
-    if len(p) > 3:
-        color = p[3]
-    if len(p) > 4:
-        label = p[4]
-    return value, name, orderRank, nodeType, color, label
+def parse(data):
+    value = data[0]
+    args = data[1]
+    name = args[0]
+    orderRank = args[1]
+    nodeType, color, label1, label2 = None, None, data[2], data[3]
+    if len(args) > 2:
+        nodeType = args[2]
+    if len(args) > 3:
+        color = args[3]
+    return value, name, orderRank, nodeType, color, label1, label2
     
 
-def createNode(rawLine, parent):
+def createNode(data, parent):
     n = Node()
-    n.value, n.name, n.orderRank, n.nodeType, n.color, n.label = parse(rawLine)
+    n.value, n.name, n.orderRank, n.nodeType, n.color, n.label1, n.label2 = parse(data)
     if parent != None:
         parent.children.append(n)
     return n
 
-def appendChildren(lines,node,length):
-    for l in lines:
-        if node.value in l[0] and len(l[0]) == length:
-            new = createNode(l, node)
-            appendChildren(lines, new, length+2)
+def appendChildren(data,node,length):
+    for d in data:
+        if node.value in d[0] and len(re.compile(r"[a-z,A-Z]").findall(d[0])) == length:
+            new = createNode(d, node)
+            appendChildren(data, new, length+1)
 
-def buildTree(lines):
+def buildTree(data):
     tree = Tree()
-    tree.root = createNode(lines[0], None)
-    appendChildren(lines, tree.root, 4)
+    tree.root = createNode(data[0], None)
+    appendChildren(data, tree.root, 2)
     return tree
     
 def mGap(gap):
@@ -77,9 +98,12 @@ def writeTreeData(node, f, gap):
     elif node.color == "b":
         f.write(",\n")
         f.write(mGap(gap) + '"nodeColor" : ' + '"' + "blank" + '"')        
-    if node.label != None:
+    if node.label1 != None:
         f.write(",\n")
-        f.write(mGap(gap) + '"label" : ' + '"' + node.label + '"')
+        f.write(mGap(gap) + '"label1" : ' + '"' + node.label1 + '"')
+    if node.label2 != None:
+        f.write(",\n")
+        f.write(mGap(gap) + '"label2" : ' + '"' + node.label2 + '"')
     if node.children != []:
         f.write(",\n")
         f.write(mGap(gap) + '"children" : ' + "[\n")
@@ -91,11 +115,11 @@ def writeTreeData(node, f, gap):
             f.write(mGap(gap) + "\n}")
         f.write(mGap(gap) + "\n]\n")
     
-def getLongestNode(lines):
-    longest = lines[0][0]
-    for l in lines:
-        if len(l[0]) > len(longest):
-            longest = l[0]
+def getLongestNode(data):
+    longest = data[0][0]
+    for d in data:
+        if len(d[0]) > len(longest):
+            longest = d[0]
     return longest
 
 def writeLabels(node, f, longest):
@@ -113,14 +137,16 @@ def writeHtml(f, comments):
     for line in lines:
         if not ("mydata.js" in line or "visual.js" in line or line == lines[len(lines)-1]):
             f.write(line + "\n")
-    if comments != []:
-        f.write("<body>" + "\n")
-        f.write("<textarea rows=" + '"' + "3" + '"' + "cols=" + '"' + "80" + '"' + ">" + "\n")
-        for comment in comments:
-            f.write(comment + '\n')
-        f.write("</textarea>" + "\n")
-        f.write("</body>" + "\n")
-    
+
+def writeComments(comments):
+    bigString = ""
+    for comment in comments:
+        bigString = bigString + comment + "\\" + "n" 
+    return bigString
+
+def writeDescription(f, comments):
+    f.write("document.getElementById(" + '"' + "description" + '"' + ").innerHTML = " + '"' + writeComments(comments) + '"' + ";\n")
+
 def readJavaScript():
     f = open("visual.js", "r")
     return f.read().splitlines()
@@ -145,26 +171,23 @@ def writeToFile(root, fileName, longest, comments):
     f.write("};\n")
 
     writeJavaScript(f)
+    writeDescription(f, comments)
     
     f.write("</script>\n")
     f.write("</html>")
          
 def printTree(n,c):
-    print c* " ", n.value
-    print c* " ", "{"
+    print 2*c * " " + n.value
     for i in n.children:
-        print 4*c* " ", i.value
-    print c* " ", "}"
-    for i in n.children:
-        printTree(i,c+1)
+        printTree(i, 2*(c+1))
 
 def main(argv):
     configFile = "config.txt"
     if len(argv) == 3:
         configFile = argv[2]    
-    lines, comments = readData(configFile)
-    tree = buildTree(lines)    
-    writeToFile(tree.root, argv[1], getLongestNode(lines), comments)
+    data, comments = readData(configFile)
+    tree = buildTree(data)
+    writeToFile(tree.root, argv[1], getLongestNode(data), comments)
     print argv[1] + " has been created" 
     
 if __name__ == "__main__":
